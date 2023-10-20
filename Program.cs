e0a2763e8d9705d0e122920c6f1d293aeb2d3560
Tales Mariano <tales.mariano1@gmail.com>
2023-10-20 14:35:58 -0300

    Initial Project Files, Class 01 Files

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Hello, what is your name?");
+
+var name = Console.ReadLine();
+
+if (name == String.Empty)
+{
+    name = "Unknown";
+}
+
+Console.WriteLine("Welcome {0} to your OOP adventure!", name);
